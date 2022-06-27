c51e1512ccabce29e8d38333632be8838339af7c
bingyue <99388013+bingqiu456@users.noreply.github.com>
2022-06-28 07:24:19 +0800

    修复编码问题
    修复了UnicodeDecodeError: 'gbk' codec can't decode byte 0xbf in position 2: illegal multibyte sequence

--- install_requirements.py
+++ install_requirements.py
@@ -4,12 +4,11 @@
 执行此文件将自动安装requirements.txt中的依赖
 '''
 import os,platform
-f=open('requirements.txt')
+f=open('requirements.txt',"r+",encoding="utf_8")
 for i in f:
-    if i[0]=='#':
+    if i.strip("\ufeff")[0]=='#':
         pass
     else:
         os.system('pip3 install -i https://pypi.tuna.tsinghua.edu.cn/simple '+i)
 f.close()
 if platform.system()=='Linux':print('请手动安装brotli')
-
